6d76d2f168f57263a2fb349a67047f7542c98f85
Vali88 <vali666@gmx.at>
2013-11-21 13:30:10 +0100

    Update censusmap-simple.js

--- page/censusmap-simple.js
+++ page/censusmap-simple.js
@@ -5,7 +5,7 @@ var osmLayer = new ol.layer.Tile({source: new ol.source.OSM()});
 var wmsLayer = new ol.layer.Image({
   source: new ol.source.ImageWMS({
     url: '/geoserver/wms',
-    params: {'LAYERS': 'g01_2013:g01_2013normalized'}
+    params: {'LAYERS': 'g01_2013:g01_2013_normalized'}
   }),
   opacity: 0.6
 });
